b83c9fdd1e771ca1b058a5267e1566fe6baceefb
delasimensah <mensadelasi@gmail.com>
2020-10-12 15:30:25 +0000

    initial setup

---
+++ pages/_app.js
@@ -0,0 +1,19 @@
+import Head from "next/head";
+
+import "../styles/globals.css";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <title>Delasi Mensah Portfolio</title>
+        <meta name="description" content="Portfolio website of Delasi Mensah" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
---
+++ pages/index.js
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div className="">
+      <h1>Coming Soon</h1>
+    </div>
+  );
+};
+
+export default Home;
