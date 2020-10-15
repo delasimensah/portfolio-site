f090a75d698557d80dc63bd0d3f66d574c033ea7
delasimensah <mensadelasi@gmail.com>
2020-10-15 15:30:25 +0000

    added gradient background to about section

--- components/About/About.jsx
+++ components/About/About.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <section className="h-screen bg-gradient-to-r from-primary to-secondary text-white">
+      About
+    </section>
+  );
 };
 
 export default About;
