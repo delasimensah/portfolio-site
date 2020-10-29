2e86455a0f24d98978aa79ecc7ac7f079afaf86f
delasimensah <mensadelasi@gmail.com>
2020-10-29 16:30:25 +0000

    updates

--- utils/projects.js
+++ utils/projects.js
@@ -3,8 +3,8 @@ export const projects = [
     title: "Social Media Website",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur quo molestias architecto eligendi, facere doloremque ab ad recusandae reprehenderit accusamus.",
-    demo: "https://delasimensah.netlify.app/",
-    github: "https://github.com/delasimensah",
+    demo: "https://social-media-website.netlify.app/",
+    github: "https://github.com/delasimensah/social-media-site",
     image: "/projects/proj1.png",
   },
 ];
