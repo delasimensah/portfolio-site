5a987ed05c91074d822730878e5ee178a0b79d62
delasimensah <mensadelasi@gmail.com>
2020-12-28 16:30:25 +0000

    updates

--- utils/projects.js
+++ utils/projects.js
@@ -2,7 +2,7 @@ export const projects = [
   {
     title: "Social Media Website",
     description:
-      "Social media website with the ability to create posts, upload images, like posts, comment on posts, follow other users and chat with friends. Built with React, Nextjs and Firebase.",
+      "Social media website with the ability to create posts, upload images, like posts, comment on posts and follow other users. Built with React, Nextjs and Firebase.",
     demo: "https://social-media-website.netlify.app/",
     github: "https://github.com/delasimensah/social-media-site",
     image: "/projects/proj1.png",
