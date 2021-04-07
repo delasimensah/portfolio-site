085406e4c4b4921d7804501b45b261aaec492bc3
delasimensah <mensadelasi@gmail.com>
2021-04-07 16:30:25 +0000

    updates

--- utils/projects.js
+++ utils/projects.js
@@ -1,9 +1,17 @@
 export const projects = [
   {
+    title: "Audiophile",
+    description:
+      "Audiophile is the premier store for high end headphones, earphones, speakers, and audio accessories. Built with React and Nextjs",
+    demo: "https://audiophile-dm.netlify.app/",
+    github: "https://github.com/delasimensah/audiophile",
+    image: "/projects/proj7.png",
+  },
+  {
     title: "Social Media Website",
     description:
       "Social media website with the ability to register users, create posts, upload images, like posts, comment on posts and follow other users. Built with React, Nextjs and Firebase.",
-    demo: "https://social-media-website.netlify.app/",
+    demo: "https://social-dm.netlify.app/",
     github: "https://github.com/delasimensah/social-media-site",
     image: "/projects/proj1.png",
   },
@@ -11,7 +19,7 @@ export const projects = [
     title: "Designo",
     description:
       "Designo is an award-winning custom design and digital branding company. This is their portfolio website showcasing their impressive designs. Built with React and Nextjs.",
-    demo: "https://designo.digitalnomadgh.com/",
+    demo: "https://designo-dm.netlify.app/",
     github: "https://github.com/delasimensah/designo",
     image: "/projects/proj2.png",
   },
@@ -19,7 +27,7 @@ export const projects = [
     title: "Photosnap",
     description:
       "A marketing website for Photosnap, a photo sharing platform for photographers and visual story tellers. Built with React and Nextjs.",
-    demo: "https://photosnap.digitalnomadgh.com/",
+    demo: "https://photosnap-dm.netlify.app/",
     github: "https://github.com/delasimensah/photosnap",
     image: "/projects/proj3.png",
   },
@@ -27,7 +35,7 @@ export const projects = [
     title: "myteam",
     description:
       "Myteam is a platform that helps companies tap into the abandunce of global talent to find and build high performing teams. Buit with React and Nextjs.",
-    demo: "https://myteam.digitalnomadgh.com/",
+    demo: "https://myteam-dm.netlify.app/",
     github: "https://github.com/delasimensah/myteam",
     image: "/projects/proj4.png",
   },
@@ -35,7 +43,7 @@ export const projects = [
     title: "dine",
     description:
       "Dine is a beautiful farmhouse restaurant in the countryside of the UK. They host family gatherings, special events and larger social events. Built with React and Gatsby. ",
-    demo: "https://dine.digitalnomadgh.com/",
+    demo: "https://dine-dm.netlify.app/",
     github: "https://github.com/delasimensah/dine-restaurant",
     image: "/projects/proj5.png",
   },
